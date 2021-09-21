f742c872d03698cea31556e0d0e695485e1aabeb
Andrewcodes12 <AndrewFava@yahoo.com>
2021-09-21 12:32:57 -0400

    minor changes to alias names

--- db/models/user.js
+++ db/models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function(models) {
     User.hasMany(models.Follow, {
       foreignKey: 'followerId',
-      as: 'usersFollow'
+      as: 'userFollowing'
     });
     User.hasMany(models.Follow, {
       foreignKey: 'userId',
